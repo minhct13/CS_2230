66dc47d64945e324f1f3e0eec9d13cbbcfb5788b
Cong Minh Tran <congminht91@gmail.com>
2023-01-19 14:03:22 +0700

    update html

--- script.js
+++ script.js
@@ -5,46 +5,55 @@ const input = dropArea.querySelector('input')
 let origin_image = NaN
 
 const ori_img = document.getElementById("img-to-crop");
-ori_img.style.display = "none";
 
-button.addEventListener('click', ()=>{
+button.addEventListener('click', () => {
     input.click()
 })
 
-input.addEventListener('change', function (){
+input.addEventListener('change', function () {
     const file = this.files[0];
     origin_image = file
     showFile(file);
-    
+
 })
 
-dropArea.addEventListener('dragover', (event)=>{
+dropArea.addEventListener('dragover', (event) => {
     event.preventDefault();
     dragText.textContent = "Drop to upload image"
 })
 
 
-dropArea.addEventListener('dragleave', (event)=>{
+dropArea.addEventListener('dragleave', (event) => {
     event.preventDefault();
     dragText.textContent = "Drag and drop to upload image"
 })
 
-dropArea.addEventListener('drop', (event)=>{
+dropArea.addEventListener('drop', (event) => {
     event.preventDefault();
     const file = event.dataTransfer.files[0];
     origin_image = file
     showFile(file);
 })
 
-function showFile(file){
+function showFile(file) {
     const fileType = file.type;
     const validExtensions = ['image/jpeg', 'image/jpg', 'image/png'];
-    if (validExtensions.includes(fileType)){
+    if (validExtensions.includes(fileType)) {
         const fileReader = new FileReader();
         fileReader.onload = () => {
             const fileUrl = fileReader.result;
-            imgTag = `<img src="${fileUrl}">`
-            dropArea.innerHTML = imgTag
+            imgTag = document.createElement("img")
+            imgTag.id = "img-to-crop"
+            imgTag.src = fileUrl
+            imgTag.style="max-height: 50vh"
+            while (dropArea.firstChild) {
+                dropArea.removeChild(dropArea.lastChild);
+            }
+            dropArea.appendChild(imgTag)
+            cropper = new Cropper(imgTag, {
+                aspectRatio: 0,
+                viewMode: 0,
+            });
         }
         fileReader.readAsDataURL(file);
     }
@@ -59,41 +68,23 @@ function showFile(file){
 const submit_btn = document.getElementById('btn-1')
 const refresh_btn = document.getElementById('btn-2')
 
-submit_btn.addEventListener('click', ()=>{
+submit_btn.addEventListener('click', () => {
     const file = origin_image
+    window.open("/home.html", "_self")
     show(file)
-    addCropper()
 })
 
-function show(file){
+function show(file) {
     const fileReader = new FileReader();
     fileReader.onload = () => {
         const fileUrl = fileReader.result;
         ori_img.src = fileUrl;
         console.log(ori_img)
-        ori_img.style.display = "block";
+        // ori_img.style.display = "block";
     }
     fileReader.readAsDataURL(file);
 }
 
-refresh_btn.addEventListener('click', ()=>{
+refresh_btn.addEventListener('click', () => {
     location.reload();
 })
-
-// =========================================
-
-let cropper = NaN
-// const image_to_crop = document.getElementById("origin-image")
-function addCropper(){
-    cropper = new Cropper(ori_img, {
-        aspectRatio: 0,
-        viewMode: 0,    
-    });
-}
-// const img = document.getElementById("test")
-// cropper = new Cropper(img, {
-//     aspectRatio: 0,
-//     viewMode: 0,    
-// });
-
-
