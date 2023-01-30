b2c6dcc20ccbbd3b81f0b446d292430de9764418
minhct13 <congminht91@gmail.com>
2023-01-31 02:09:54 +0700

    update script

--- start_app.sh
+++ start_app.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 cd backend/;
-docker load --input docker/cs419.tar;
-docker run backend-app:latest -p 8000;
+
+echo "Downloading backend docker image ..."
+cd ./docker;wget https://drive.google.com/file/d/1AacIr4gZh_Utq1R2Pro0OKK3Rh8XjWpB/;
+
+echo "Loading docker image ..."
+docker load -i docker/cs419.tar;
+docker-compose -f docker/docker-compose.yml up -d
+
+echo "Downloading oxford5k dataset ..."
+cd ../../data;wget https://drive.google.com/file/d/1g3wiFb9fEv7Zt8KTyEJJnU0jcbzgvIX3/;
+
+echo "Done, now you can start retrieve via index.html"
