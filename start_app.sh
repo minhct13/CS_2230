7a398ad8c90a43464043b0664fa119ce8a312075
minhct13 <congminht91@gmail.com>
2023-01-31 02:33:15 +0700

    update script

--- start_app.sh
+++ start_app.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 cd backend/;
 
-echo "Downloading backend docker image ..."
-cd ./docker;wget https://drive.google.com/file/d/1AacIr4gZh_Utq1R2Pro0OKK3Rh8XjWpB/;
+pip install gdown
 
-echo "Loading docker image ..."
+echo "========================= [Downloading backend docker image ...] ========================="
+cd ./docker;gdown 1AacIr4gZh_Utq1R2Pro0OKK3Rh8XjWpB;
+
+echo "========================= [Loading docker image ...] ========================="
 docker load -i docker/cs419.tar;
 docker-compose -f docker/docker-compose.yml up -d
 
-echo "Downloading $ unzip oxford5k dataset ..."
-cd ../../data;wget https://drive.google.com/file/d/1g3wiFb9fEv7Zt8KTyEJJnU0jcbzgvIX3/;
+echo "========================= [Downloading $ unzip oxford5k dataset ...] ========================="
+cd ../../data;gdown 1g3wiFb9fEv7Zt8KTyEJJnU0jcbzgvIX3;
 unzip oxford5k.zip
-echo "Done, now you can start retrieve via index.html"
+echo "========================= [Done, now you can start retrieve via index.html] ========================="
