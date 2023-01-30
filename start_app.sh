97630912ce624879b91c6c75cc216ddaba7cbd2e
minhct13 <congminht91@gmail.com>
2023-01-31 03:51:38 +0700

    update script

--- start_app.sh
+++ start_app.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
+
 cd backend/;
 
 pip install gdown
 
-echo "========================= [Downloading backend docker image ...] ========================="
-cd ./docker;gdown 1AacIr4gZh_Utq1R2Pro0OKK3Rh8XjWpB;
+cd ./docker
+if [ ! -f "backend.tar" ]; then
+    echo "========================= [Downloading backend docker image ...] ========================="
+    gdown 1AacIr4gZh_Utq1R2Pro0OKK3Rh8XjWpB;
+fi
 
 echo "========================= [Loading docker image ...] ========================="
-docker load -i docker/cs419.tar;
-docker-compose -f docker/docker-compose.yml up -d
+docker load -i cs419.tar;
+docker-compose -f docker-compose.yml up -d
 
-echo "========================= [Downloading $ unzip oxford5k dataset ...] ========================="
-cd ../../data;gdown 1g3wiFb9fEv7Zt8KTyEJJnU0jcbzgvIX3;
-unzip oxford5k.zip
-echo "========================= [Done, now you can start retrieve via index.html] ========================="
+cd ../../data;
+if [ ! -f "oxford5k.zip" ]; then
+    echo "========================= [Downloading $ unzip oxford5k dataset ...] ========================="
+    gdown 1g3wiFb9fEv7Zt8KTyEJJnU0jcbzgvIX3;
+fi
+
+if [ ! -d "oxford5k/jpg" ]; then
+    apt-get install unzip
+    unzip oxford5k.zip
+fi
+
+echo "========================= [Finished.] ========================="
