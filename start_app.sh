662a359f79275de68096d7112b2edd89bf79a61a
minhct13 <congminht91@gmail.com>
2023-01-31 02:14:38 +0700

    update script

--- start_app.sh
+++ start_app.sh
@@ -8,7 +8,7 @@ echo "Loading docker image ..."
 docker load -i docker/cs419.tar;
 docker-compose -f docker/docker-compose.yml up -d
 
-echo "Downloading oxford5k dataset ..."
+echo "Downloading $ unzip oxford5k dataset ..."
 cd ../../data;wget https://drive.google.com/file/d/1g3wiFb9fEv7Zt8KTyEJJnU0jcbzgvIX3/;
-
+unzip oxford5k.zip
 echo "Done, now you can start retrieve via index.html"
